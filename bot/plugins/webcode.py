6e888d927819deab1319b4a4126e69b295dace9e
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-04-29 21:31:49 +0530

    Create webcode.py

---
+++ bot/plugins/webcode.py
@@ -0,0 +1,12 @@
+from aiohttp import web as webserver
+
+routes = webserver.RouteTableDef()
+
+async def bot_run():
+    _app = webserver.Application(client_max_size=30000000)
+    _app.add_routes(routes)
+    return _app
+  
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return webserver.json_response("GOUTHAMSER")
