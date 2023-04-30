15f31242c560555ff271111ed350a814e82c04ee
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-04-30 09:53:34 +0530

    myr file size

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -19,7 +19,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + file_size "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
         try:
             await update.reply_cached_media(
                 file_id,
