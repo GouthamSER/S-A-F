8fb9cd4df3e0fcedc6efaeb05083296e0d6cc73d
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-05-01 15:43:48 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -19,7 +19,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>"), ("<code>" + file_size + "</code>")
         
         try:
             await update.reply_cached_media(
