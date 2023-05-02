68747b653a9355d09428365681b8bdf9e178e422
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-05-02 13:20:25 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -19,7 +19,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
      #CUSTOM FILE CAPTION   
-        caption = file_caption if file_caption != ("" or None) else ("<i><u>File Name:</u>" + file_name + "</i>")
+        caption = file_caption if file_caption != ("" or None) else ("<i><u>File Name:</u>" + file_name + "</i>\n\nReq Movie Here : @wudixh")
         size = file_size if file_size != ("" or None) else ("<code>" + file_size + "</code>")
         
         try:
