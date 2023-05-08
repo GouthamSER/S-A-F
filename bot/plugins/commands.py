bb067530fa37cfe8073ae039da9e79a1a1bb18db
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-05-08 22:03:03 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -49,9 +49,6 @@ async def start(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        m=await message.reply_sticker("CAACAgUAAxkBAAIuc2OxMvp4oKa3eqg6zBTCZZdtxFV3AAIvAAPhAAEBGxa4Kik7WjyMHgQ")  #STICKER ADDED BY GOUTHAM SER
-        await asyncio.sleep(1)
-        await m.delete()
         text=Translation.START_TEXT.format(update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode=enums.ParseMode.HTML,
