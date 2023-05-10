e8b9c0036d0431ceaa913b1426a476bf40b25c7b
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-05-10 17:01:39 +0530

    ADDED EMOJI IN RESULT

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,8 +81,8 @@ async def auto_filter(bot, update):
             
             file_size = "" if file_size == ("[0 B]") else file_size
             
-            # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            # add emoji down below inside " " if you want.. BUTTON RESULT EMOJI ADDED
+            button_text = f"🎭{file_size}🔸{file_name}"
             
 
             if file_type == "video":
